87794391fe7e3f54a44758d6db68dee5164059ce
Ian Chen <ian@Ians-MacBook-Pro.local>
2019-03-15 13:22:14 +0800

    update index

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import json
+
 from flask import Flask, render_template
+
 app = Flask(__name__)
+db = json.load(open('./data.json'))
 
 @app.route("/")
 def hello():
-    return render_template('_base.html', title="HOME")
+    return render_template('index.html', title="HOME", products=db[:10])
